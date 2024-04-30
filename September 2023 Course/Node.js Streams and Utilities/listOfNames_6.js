9651ee321d1aec49891f8944e97b5354e935032e
Dayana Uzunova <daqnauzunova@abv.bg>
2024-04-30 22:47:57 +0300

    wrong tasks.
    wrong tasks.

--- September 2023 Course/Node.js Streams and Utilities/listOfNames_6.js
+++
@@ -1,6 +0,0 @@
-function listOfNames(array){
-    array.sort((a, b) => a.localeCompare(b))
-    .forEach((value, i) => {
-        console.log(`${i + 1}.${value}`);
-    })
-}
--- September 2023 Course/Node.js Streams and Utilities/printEveryNelement_2.js
+++
@@ -1,8 +0,0 @@
-function print(array, n){
-    let result = [];
-
-    for (let i = 0; i < array.length; i += n){
-        result.push(array[i]);
-    }
-    return result;
-}
