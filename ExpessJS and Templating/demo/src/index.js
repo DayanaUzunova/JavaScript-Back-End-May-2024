b1c05751ba327239d28c46b763bc198dc7d5f7af
Dayana Uzunova <daqnauzunova@abv.bg>
2024-05-27 14:04:57 +0300

    express demo.
    express demo.

---
+++ ExpessJS and Templating/demo/src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { catalogRouter } = require('./catalog');
+const { countMiddleware } = require('./middlewares/counter');
+const { dataController } = require('./data');
+
+const app = express();
+
+const homeHtml = `
+    <h1>Home Page</h1>
+    <a href="/">Home</a>
+    <a href="/catalog">Catalog</a>
+`;
+
+const catalogHtml = `
+    <h1>Catalog</h1>
+    <a href="/">Home</a>
+    <a href="/catalog">Catalog</a>
+`;
+
+app.get('/', (req, res) => {
+    res.send(homeHtml);
+});
+
+app.use('/catalog', catalogRouter);
+
+app.get('/data', countMiddleware, dataController);
+
+app.get('/catalog/:category/:productId', (req, res) => {
+    res.send(catalogHtml + `<h2>${req.params.category}</h2>`
+        + `<p>ItemId: ${req.params.productId}</p>`
+);
+});
+
+app.get('*', (req, res) => {
+    res.status(404);
+    res.send('404 FILE NOT FOUND!')
+});
+
+app.listen(3000, () => {
+    console.log('App is listening on port 3000');
+});
